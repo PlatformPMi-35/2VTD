e2ba9a3a70fe81961f50a202b9797f8f94f9c662
Vlad <38084885+Vladacdc@users.noreply.github.com>
2018-09-15 20:57:47 +0300

    enum Color created

---
+++ FirstTask/FirstTask/Library/Color.cs
@@ -0,0 +1,16 @@
+﻿namespace FirstTask.Library
+{
+    internal enum Color
+    {
+        Black = 0,
+        Red,
+        Orange,
+        Yelow,
+        Green,
+        Cyan,
+        Blue,
+        Purple,
+        White,
+        Pink,
+    }
+}
