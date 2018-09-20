529cffe01ddc687fb8948fe6acfefcfe49d2e881
Vlad <38084885+Vladacdc@users.noreply.github.com>
2018-09-20 20:43:19 +0300

    Xml

--- FirstTask/FirstTask/Library/Triangle.cs
+++ FirstTask/FirstTask/Library/Triangle.cs
@@ -4,25 +4,41 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    class Triangle
+    /// <summary>
+    /// Represents <see cref="Triangle"/>.
+    /// </summary>
+    internal class Triangle
     {
+        /// <summary>
+        /// Has ColorSides.
+        /// </summary>
         private ColorSide[] sides;
-
+       
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Triangle" /> class and sets all <see cref="ColorSide"/> to <see cref="Color.Black"/>.
+        /// </summary>
         public Triangle()
         {
-            sides = new ColorSide[3];
+            this.sides = new ColorSide[3];
             for (int i = 0; i < 3; i++)
             {
-                sides[i] = new ColorSide(Color.Black);
+                this.sides[i] = new ColorSide(Color.Black);
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Triangle" /> class and sets all it Three <see cref="ColorSide"/>.
+        /// </summary>
+        /// <param name="sideA">Represent First <see cref="Triangle"/> <see cref="ColorSide"/> will be set.</param>
+        /// <param name="sideB">Represent Second <see cref="Triangle"/> <see cref="ColorSide"/> will be set.</param>
+        /// <param name="sideC">Represent Third <see cref="Triangle"/> <see cref="ColorSide"/> will be set.</param>
+        /// <exception cref="ArgumentException">Invalid Argument.</exception>
         public Triangle(ColorSide sideA, ColorSide sideB, ColorSide sideC)
         {
             try
             {
-                sides = new ColorSide[3];
-                SetSides(sideA, sideB, sideC);
+                this.sides = new ColorSide[3];
+                this.SetSides(sideA, sideB, sideC);
             }
             catch (Exception)
             {
@@ -30,40 +46,11 @@
             }
         }
 
-        private static bool IsTriang(ColorSide sideA, ColorSide sideB, ColorSide sideC)
-        {
-            if (sideA.Length + sideB.Length <= sideC.Length ||
-                            sideA.Length + sideC.Length <= sideB.Length ||
-                            sideB.Length + sideC.Length <= sideA.Length)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        public ColorSide[] GetSides()
-        {          
-            return (ColorSide[])sides.Clone();
-        }
-
-        public void SetSides(ColorSide sideA, ColorSide sideB, ColorSide sideC)
-        {
-            if (IsTriang(sideA, sideB, sideC) == false)
-            {
-                throw new ArgumentException("Transmitted sides can not make a triangle");
-            }
-
-            sides[0] = sideA;
-            sides[1] = sideB;
-            sides[2] = sideC;
-        }
-
-        public override string ToString()
-        {
-            return String.Format("{0} {1} {2} {3} {4} {5}", 
-                sides[0].Color, sides[0].Length, sides[1].Color, sides[1].Length, sides[2].Color, sides[2].Length);
-        }
-
+        /// <summary>
+        /// Transforms <see cref="string"/> into <see cref="Triangle"/>.
+        /// </summary>
+        /// <param name="str"> <see cref="string"/> that will be transformed into <see cref="Triangle"/>.</param>
+        /// <exception cref="FormatException">Ivalid Format.</exception>
         public static explicit operator Triangle(string str)
         {
             List<string> buff = str.Split().ToList();
@@ -73,7 +60,8 @@
                 Color color2 = (Color)Enum.Parse(typeof(Color), buff[2]);
                 Color color3 = (Color)Enum.Parse(typeof(Color), buff[4]);
 
-                return new Triangle( new ColorSide(color1, Convert.ToInt32(buff[1])),
+                return new Triangle(
+                    new ColorSide(color1, Convert.ToInt32(buff[1])),
                     new ColorSide(color2, Convert.ToInt32(buff[3])),
                     new ColorSide(color3, Convert.ToInt32(buff[5])));
             }
@@ -81,6 +69,68 @@
             {
                 throw new FormatException("Transmitted string is in wrong format");
             }
+        }
+
+        /// <summary>
+        /// Clones <see cref="ColorSide"/>s of <see cref="Triangle"/>.
+        /// </summary>
+        /// <returns>Cloned <see cref="ColorSide"/>s of <see cref="Triangle"/>.</returns>
+        public ColorSide[] GetSides()
+        {          
+            return (ColorSide[])this.sides.Clone();
+        }
+
+        /// <summary>
+        /// Sets All Three <see cref="ColorSide"/>s of <see cref="Triangle"/>.
+        /// </summary>
+        /// <param name="sideA">Represent First <see cref="Triangle"/> <see cref="ColorSide"/> will be set.</param>
+        /// <param name="sideB">Represent Second <see cref="Triangle"/> <see cref="ColorSide"/> will be set.</param>
+        /// <param name="sideC">Represent Third <see cref="Triangle"/> <see cref="ColorSide"/> will be set.</param>
+        public void SetSides(ColorSide sideA, ColorSide sideB, ColorSide sideC)
+        {
+            if (IsTriang(sideA, sideB, sideC) == false)
+            {
+                throw new ArgumentException("Transmitted sides can not make a triangle");
+            }
+
+            this.sides[0] = sideA;
+            this.sides[1] = sideB;
+            this.sides[2] = sideC;
+        }
+
+        /// <summary>
+        /// Override <see cref="object.ToString()"/>.
+        /// </summary>
+        /// <returns>String adaptation of the <see cref="Triangle"/>. <seealso cref="object.ToString()"/>.</returns>
+        public override string ToString()
+        {
+            return string.Format(
+                "{0} {1} {2} {3} {4} {5}", 
+                this.sides[0].Color, 
+                this.sides[0].Length, 
+                this.sides[1].Color, 
+                this.sides[1].Length, 
+                this.sides[2].Color, 
+                this.sides[2].Length);
+        }
+
+        /// <summary>
+        /// Checks if Three <see cref="ColorSide"/>s complete a <see cref="Triangle"/>.
+        /// </summary>
+        /// <param name="sideA">Represent First <see cref="Triangle"/> <see cref="ColorSide"/> will be checked.</param>
+        /// <param name="sideB">Represent Second <see cref="Triangle"/> <see cref="ColorSide"/> will be checked.</param>
+        /// <param name="sideC">Represent Third <see cref="Triangle"/> <see cref="ColorSide"/> will be checked.</param>
+        /// <returns>True is <see cref="ColorSide"/>s complete <see cref="Triangle"/></returns>
+        private static bool IsTriang(ColorSide sideA, ColorSide sideB, ColorSide sideC)
+        {
+            if (sideA.Length + sideB.Length <= sideC.Length ||
+                            sideA.Length + sideC.Length <= sideB.Length ||
+                            sideB.Length + sideC.Length <= sideA.Length)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
