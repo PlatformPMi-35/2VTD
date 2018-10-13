9983f076b8c26c492fa853c94d3d1a9fed71dade
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-10-13 18:28:49 +0300

    added PolyLine class

--- Task2/Task2/Library/PolyLine.cs
+++ Task2/Task2/Library/PolyLine.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Task2.Library
 {
-    class PolyLine
+    public class PolyLine
     {
+        public List<Point> points;
+        public Color color;
+        
+        public PolyLine(IEnumerable<Point> points, Color color)
+        {
+            this.points = new List<Point>(points);
+            this.color = color;
+        }
+
+        public PolyLine(IEnumerable<Point> points) : this(points, Color.FromRgb(255, 255, 255)) { }
+        public PolyLine() : this(null, Color.FromRgb(255, 255, 255)) { }
+
     }
 }
