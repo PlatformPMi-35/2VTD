1fdbd40b2b2d30eea6b137d3c9c43da910d1b15d
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-10-13 22:02:21 +0300

    LineDrawingManager class added

---
+++ Task2/Task2/Library/LineDrawingManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Task2.Library
+{
+    class LineDrawingManager
+    {
+        public ObservableCollection<PolyLine> polyLines;
+
+        public LineDrawingManager()
+        {
+            polyLines = new ObservableCollection<PolyLine>();
+
+        }
+
+        //public void CreatePL(Polyline line, Color color)
+        //{
+        //    PolyLine pl = new PolyLine(line, color);
+        //    polyLines.Add(pl);
+        //}
+        //public void CreatePL(Polyline line)
+        //{
+        //    PolyLine pl = new PolyLine(line);
+        //    polyLines.Add(pl);
+        //}
+    }
+}
