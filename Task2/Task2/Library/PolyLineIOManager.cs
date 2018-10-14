9f9337c8af33586b3df61d87a5d5a749d48ab6e1
dimkijeee <dimkijeee@gmail.com>
2018-10-14 21:23:31 +0300

    re-add class

--- Task2/Task2/Library/PolyLineIOManager.cs
+++ Task2/Task2/Library/PolyLineIOManager.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 
 namespace Task2.Library
 {
