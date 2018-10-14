8757798b976938c5f06e85a298bb4d004c119bc2
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-10-14 19:04:19 +0300

    added PolyLineIOManager

---
+++ Task2/Task2/Library/PolyLineIOManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Task2.Library
+{
+    class PolyLineIOManager
+    {
+        public static void Save(IEnumerable<PolyLine> lines, string path)
+        {
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(PolyLine));
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(fs, lines);
+            }
+
+        }
+
+        public static IEnumerable<PolyLine> Load(string path)
+        {
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(PolyLine));
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                IEnumerable<PolyLine> lines = (IEnumerable<PolyLine>)jsonFormatter.ReadObject(fs);
+
+                return lines;
+            }
+        }
+    }
+}
