d6fc040f6bb7a13f7f57cdf6207d7050b9cfa2fc
dimkijeee <dimkijeee@gmail.com>
2018-10-16 00:17:32 +0300

    add some tests

--- Task2/UnitTests/PolyLineTests.cs
+++ Task2/UnitTests/PolyLineTests.cs
@@ -2,6 +2,7 @@
 using Task2.Library;
 using System.Collections.Generic;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace UnitTests
 {
@@ -31,11 +32,47 @@ namespace UnitTests
         [TestMethod]
         public void TestAddingToPLManager2()
         {
+            LineDrawingManager manager = new LineDrawingManager();
+            Polyline polyLine = new Polyline();
+            polyLine.Points.Add(new System.Windows.Point(1, 3));
+            polyLine.Points.Add(new System.Windows.Point(2, 4));
+            manager.AddPl(polyLine, Brushes.Aqua);
 
+            Assert.IsTrue(manager.Polylines.Contains(new PolyLine(polyLine.Points, Brushes.Aqua)));
         }
 
         [TestMethod]
         public void TestAddingToPLManager3()
+        {
+            LineDrawingManager manager = new LineDrawingManager();
+            Polyline polyLine = new Polyline();
+            polyLine.Points.Add(new System.Windows.Point(1, 3));
+            polyLine.Points.Add(new System.Windows.Point(2, 4));
+            manager.AddPl(polyLine);
+
+            Assert.IsTrue(manager.Polylines.Contains(new PolyLine(polyLine.Points)));
+        }
+
+        [TestMethod]
+        public void TestIOManager1()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestIOManager2()
+        {
+
+        }
+
+        [TestMethod]
+        public void BrushToStrConverterTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PointCollectionToStrConverterTest()
         {
 
         }
