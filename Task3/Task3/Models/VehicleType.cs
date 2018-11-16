9d3cb534e4d07a173a6d164396f461276fe35533
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-11-16 23:26:43 +0200

    added VehicleType model

---
+++ Task3/Task3/Models/VehicleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3.Models
+{
+    public enum VehicleType
+    {
+        Curtainsider,
+        Box,
+        Minibus,
+        Refridgerator,
+        Dropside,
+        Platform,
+        Tank,
+        Special
+    }
+}
