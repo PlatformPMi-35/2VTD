09740aef6e726bf50cfdebc18b3830b8cb3a8416
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-11-16 23:26:15 +0200

    added Vehicle model

---
+++ Task3/Task3/Models/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿namespace Task3.Models
+{
+    public class Vehicle
+    {
+        private double weight;
+        private double volume;
+
+        public VehicleType Type { get; set; }
+        public string NumberPlate { get; set; }
+
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+            }
+        }
+        public double Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    volume = value;
+                }
+            }
+        }
+
+        public Vehicle(string NumberPlate, VehicleType Type, double Weight, double Volume)
+        {
+            this.NumberPlate = NumberPlate;
+            this.Type = Type;
+            this.Weight = Weight;
+            this.Volume = Volume;
+        }
+    }
+}
