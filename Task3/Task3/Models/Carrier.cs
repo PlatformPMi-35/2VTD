86759e06d9b8057e390f42c11ed7d93c01206fdc
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-11-16 23:26:00 +0200

    Added Carrier model

---
+++ Task3/Task3/Models/Carrier.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3.Models
+{
+    class Carrier
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
