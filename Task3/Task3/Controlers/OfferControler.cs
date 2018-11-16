99f90f9457e719a5056a891627869541c75050c8
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-11-16 23:27:38 +0200

    added Offer Controler

---
+++ Task3/Task3/Controlers/OfferControler.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using Task3.Models;
+
+namespace Task3.Controlers
+{
+    class OfferControler
+    {
+        List<Offer> offers;
+
+        public OfferControler(List<Offer> lo)
+        {
+            offers = lo;
+        }
+        public OfferControler() : this(new List<Offer>()) { }
+
+        public void AddOffers(Offer offer)
+        {
+            offers.Add(offer);
+        }
+
+        public void RemoveOffers(Offer offer)
+        {
+            offers.Remove(offer);
+        }
+
+        public List<Offer> GetOffers()
+        {
+            return offers;
+        }
+        public List<Offer> GetOffers(Filter filter)
+        {
+            List<Offer> res = new List<Offer>();
+
+            foreach (Offer o in offers)
+            {
+                if (filter.MinDateOfPosting != null)
+                {
+                    if (o.DateOfPosting < filter.MinDateOfPosting)
+                    {
+                        continue;
+                    }
+                }
+                if (filter.MaxDateOfPosting != null)
+                {
+                    if (o.DateOfPosting > filter.MaxDateOfPosting)
+                    {
+                        continue;
+                    }
+                }
+
+                if (filter.From != null)
+                {
+                    if (o.From != filter.From)
+                    {
+                        continue;
+                    }
+                }
+                if (filter.To != null)
+                {
+                    if (o.To != filter.To)
+                    {
+                        continue;
+                    }
+                }
+
+                if (filter.MinDateOfLoading != null)
+                {
+                    if (o.DateOfLoading < filter.MinDateOfLoading)
+                    {
+                        continue;
+                    }
+                }
+                if (filter.MaxDateOfLoading != null)
+                {
+                    if (o.DateOfLoading > filter.MaxDateOfLoading)
+                    {
+                        continue;
+                    }
+                }
+
+                if(filter.Type != null)
+                {
+                    if(o.VehicleInfo.Type != filter.Type)
+                    {
+                        continue;
+                    }
+                }
+
+                if (filter.MinWeight != null)
+                {
+                    if (o.VehicleInfo.Weight < filter.MinWeight)
+                    {
+                        continue;
+                    }
+                }
+                if (filter.MaxWeight != null)
+                {
+                    if (o.VehicleInfo.Weight > filter.MaxWeight)
+                    {
+                        continue;
+                    }
+                }
+
+                if (filter.MinVolume != null)
+                {
+                    if (o.VehicleInfo.Volume < filter.MinVolume)
+                    {
+                        continue;
+                    }
+                }
+                if (filter.MaxVolume != null)
+                {
+                    if (o.VehicleInfo.Volume > filter.MaxVolume)
+                    {
+                        continue;
+                    }
+                }
+
+                res.Add(o);
+            }
+
+            return res;
+        }
+    }
+}
