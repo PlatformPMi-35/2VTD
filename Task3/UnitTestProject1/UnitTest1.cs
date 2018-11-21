2933835cab3ba512c4192aa62f59b87238b06143
Vlad <38084885+Vladacdc@users.noreply.github.com>
2018-11-22 00:24:50 +0200

    Added Tests

---
+++ Task3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3.Controllers;
+using Task3.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestIOController
+    {
+        [TestMethod]
+        public void SaveLoad()
+        {
+            int id = 0;
+            DateTime dateOfPosting = DateTime.Now;
+            string from = "Ukraine";
+            string to = "Poland";
+            DateTime dateOfLoading = DateTime.Now;
+            Vehicle vehicle = new Vehicle(VehicleType.Box, 25);
+            Carrier carrier = new Carrier("John", "+123456789", "smith@gmail.com", vehicle);
+
+            Offer offer = new Offer(id, dateOfPosting, from, to, dateOfLoading, vehicle, carrier);
+
+            IOConroller.SaveOffer(@"../../TestSave.bin", offer);
+            var readed = IOConroller.LoadOffer(@"../../TestSave.bin")[0];
+
+            Assert.IsTrue(
+                readed.Id == id &&
+                readed.DateOfPosting == dateOfPosting &&
+                readed.From == from &&
+                readed.To == to &&
+                readed.DateOfLoading == dateOfLoading &&
+                readed.VehicleInfo.Type == vehicle.Type &&
+                readed.VehicleInfo.Weight == vehicle.Weight &&
+                readed.CarrierInfo.Name == carrier.Name &&
+                readed.CarrierInfo.PhoneNumber == carrier.PhoneNumber &&
+                readed.CarrierInfo.Email == carrier.Email &&
+                readed.CarrierInfo.Vehicle.Type == vehicle.Type &&
+                readed.CarrierInfo.Vehicle.Weight == vehicle.Weight);
+
+        }
+    }
+
+    [TestClass]
+    public class TestOfferController
+    {
+        [TestMethod]
+        public void GetOffersFilter()
+        {
+            int id = 0;
+            DateTime dateOfPosting = DateTime.Now;
+            string from = "Ukraine";
+            string to = "Poland";
+            DateTime dateOfLoading = DateTime.Now;
+            Vehicle vehicle = new Vehicle(VehicleType.Box, 25);
+            Carrier carrier = new Carrier("John", "+123456789", "smith@gmail.com", vehicle);
+
+            Offer offer = new Offer(id, dateOfPosting, from, to, dateOfLoading, vehicle, carrier);
+
+            Filter filter = new Filter(minDateOfPosting: dateOfPosting, 
+                from: from, 
+                type: vehicle.Type,
+                maxWeight: vehicle.Weight
+                );
+
+            OfferController offerController = new OfferController();
+            offerController.AddOffer(offer);
+            var res = offerController.GetOffers(filter);
+            Assert.IsTrue(res.Count == 1);
+        }
+    }
+}
