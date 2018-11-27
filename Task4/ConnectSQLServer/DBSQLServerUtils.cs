51071fe3438a7e99d13cb084273415a1fe27e489
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-11-27 21:35:02 +0200

    Established connection with database Closes#46

---
+++ Task4/ConnectSQLServer/DBSQLServerUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+namespace Tutorial.SqlConn
+{
+    class DBSQLServerUtils
+    {
+        public static SqlConnection GetDBConnection(string datasource, string database, string username, string password)
+        {
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            return conn;
+        }
+    }
+}
---
+++ Task4/ConnectSQLServer/DBUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace Tutorial.SqlConn
+{
+    class DBUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string datasource = @"(localdb)\MSSQLLocalDB";
+
+            string database = "Northwind";
+            string username = "";
+            string password = "";
+
+            return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
+        }
+    }
+
+}
