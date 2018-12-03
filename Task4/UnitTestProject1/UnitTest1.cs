48a607093692118b950fa641d9241faa710740b0
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-12-03 11:47:02 +0200

    Created UnitTest proj

---
+++ Task4/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task4.Controllers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDBConnection()
+        {
+            Assert.IsTrue(DBUtils.TryConnect());
+        }
+    }
+}
