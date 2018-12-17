6f51297651bd1ce442889ed6c389e30599dbebc3
UkhachevychT <30273529+UkhachevychT@users.noreply.github.com>
2018-12-17 18:48:57 +0200

    added Unit tests Closes: #54

--- Task3/UnitTestProject1/UnitTest1.cs
+++ Task3/UnitTestProject1/UnitTest1.cs
@@ -18,28 +18,41 @@ namespace UnitTestProject1
             string to = "Poland";
             DateTime dateOfLoading = DateTime.Now;
             Vehicle vehicle = new Vehicle(1, VehicleType.Box, 25);
-            Carrier carrier = new Carrier(1, "John", "+123456789", "smith@gmail.com", vehicle);
+            Carrier carrier = new Carrier(1, "John", "+123456789", "smith@gmail.com", 1);
+            Offer offer = new Offer(id, dateOfPosting, from, to, dateOfLoading, 1);
 
-            Offer offer = new Offer(id, dateOfPosting, from, to, dateOfLoading, vehicle, carrier);
-            List<Offer> l = new List<Offer>();
-            l.Add(offer);
-            IOConroller.SaveOffer(@"../../TestSave.bin", l);
-            var readed = IOConroller.LoadOffer(@"../../TestSave.bin")[0];
+            UnitOfWork uow = new UnitOfWork("TestDb");
+            uow.Vehicles.Create(vehicle);
+            uow.Carriers.Create(carrier);
+            uow.Offers.Create(offer);
+            uow.Save();
+
+            Vehicle v = uow.Vehicles.Get(1);
+            Carrier c = uow.Carriers.Get(1);
+            Offer o = uow.Offers.Get(1);
+
+            uow.Vehicles.Delete(1);
+            uow.Carriers.Delete(1);
+            uow.Carriers.Delete(1);
+            uow.Save();
 
             Assert.IsTrue(
-                //readed.Id == id &&
-                readed.DateOfPosting == dateOfPosting &&
-                readed.From == from &&
-                readed.To == to &&
-                readed.DateOfLoading == dateOfLoading &&
-                readed.VehicleInfo.Type == vehicle.Type &&
-                readed.VehicleInfo.Weight == vehicle.Weight &&
-                readed.CarrierInfo.Name == carrier.Name &&
-                readed.CarrierInfo.PhoneNumber == carrier.PhoneNumber &&
-                readed.CarrierInfo.Email == carrier.Email &&
-                readed.CarrierInfo.Vehicle.Type == vehicle.Type &&
-                readed.CarrierInfo.Vehicle.Weight == vehicle.Weight);
+                o.OfferId == offer.OfferId &&
+                o.DateOfPosting == offer.DateOfPosting &&
+                o.From == offer.From &&
+                o.To == offer.To &&
+                o.DateOfLoading == offer.DateOfLoading &&
+                o.CarrierId == offer.CarrierId &&
 
+                c.CarrierId == carrier.CarrierId &&
+                c.Name == carrier.Name &&
+                c.PhoneNumber == carrier.PhoneNumber &&
+                c.Email == carrier.Email &&
+                c.VehicleId == carrier.VehicleId &&
+
+                v.VehicleId == vehicle.VehicleId &&
+                v.Type == vehicle.Type &&
+                v.Weight == vehicle.Weight);
         }
     }
 
@@ -54,17 +67,40 @@ namespace UnitTestProject1
             string from = "Ukraine";
             string to = "Poland";
             DateTime dateOfLoading = DateTime.Now;
-            Vehicle vehicle = new Vehicle(1, VehicleType.Box, 25);
-            Carrier carrier = new Carrier(1, "John", "+123456789", "smith@gmail.com", vehicle);
-
-            Offer offer = new Offer(id, dateOfPosting, from, to, dateOfLoading, vehicle, carrier);
-
-            Filter filter = new Filter(minDateOfPosting: dateOfPosting, 
+            Vehicle vehicle = new Vehicle()
+            {
+                VehicleId = 1,
+                Type = VehicleType.Box,
+                Weight = 25
+            };
+            Carrier carrier = new Carrier()
+            {
+                CarrierId = 1,
+                Name = "John",
+                Email = "smith@gmail.com",
+                PhoneNumber = "+123456789",
+                VehicleId = 1,
+                Vehicle = vehicle
+            };
+            Offer offer = new Offer()
+            {
+                OfferId = id,
+                DateOfLoading = dateOfLoading,
+                DateOfPosting = dateOfPosting,
+                From = from,
+                To = to,
+                CarrierId = 1,
+                Carrier = carrier
+            };
+            
+            Filter filter = new Filter(
+                
+                minDateOfPosting: dateOfPosting,
                 from: from, 
                 type: vehicle.Type,
                 maxWeight: vehicle.Weight
                 );
-
+            
             OfferController offerController = new OfferController();
             offerController.AddOffer(offer);
             var res = offerController.GetOffers(filter);
